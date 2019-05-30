219f95dd3baf13fa91c10aba40279e1dc45f1225
aLehyani <lehyania@gmail.com>
2019-05-30 04:08:07 +0100

    create team teamLeader admin

--- Back/stagiaire/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO stagiaire (id, cin, date_naissance, email, nom, prenom, rib, tel) VALUES
-(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '11111111', '2019-02-13', 'ghazoua.hsairi@enis.tn', 'ghazoua', 'hsairi', '00000', '25074800'),
-(3, '11223666', '2019-02-14', 'maryem@maryem.de', 'maryem', 'ben salem', '0025255505', '251105525'),
-(4, '11223666', '2019-02-14', 'maryem@maryem.de', 'maryem', 'ben salem', '0025255505', '251105525'),
-(5, '11823666', '2019-05-14', 'maem@majuryem.de', 'iaryem', 'ben alem', '0925255505', '277705525');
