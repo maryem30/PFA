4c755baf70c5c8fe2a32e22f0f4a954132de81b2
aLehyani <ahmed.lehyani@enis.tn>
2019-05-31 01:51:16 +0100

    update backend

--- Back/stagiaire/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO team (id , team_leader, team_name) VALUES (1, 'java', 'ghazoua'), (2, 'php', 'mariem');
-INSERT INTO stagiaire (id, cin, date_naissance, email, nom, prenom, rib, tel, team_id) VALUES
-(2, '11111111', '2019-02-13', 'ghazoua.hsairi@enis.tn', 'ghazoua', 'hsairi', '00000', '25074800',1),
-(3, '11223666', '2019-02-14', 'maryem@maryem.de', 'maryem', 'ben salem', '0025255505', '251105525',1),
-(4, '11223666', '2019-02-14', 'maryem@maryem.de', 'maryem', 'ben salem', '0025255505', '251105525',1),
-(5, '11823666', '2019-05-14', 'maem@majuryem.de', 'iaryem', 'ben alem', '0925255505', '277705525',1);
